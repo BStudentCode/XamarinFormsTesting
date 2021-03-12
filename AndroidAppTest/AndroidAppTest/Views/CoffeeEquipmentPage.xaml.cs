2ecb67c59cf681c09b2489a1177418fe320ea1c1
B <hennyhexservers@gmail.com>
2021-03-12 21:17:55 +0000

    working clicker app

--- AndroidAppTest/AndroidAppTest/Views/CoffeeEquipmentPage.xaml.cs
+++ AndroidAppTest/AndroidAppTest/Views/CoffeeEquipmentPage.xaml.cs
@@ -15,6 +15,18 @@ namespace AndroidAppTest.Views
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
+
+            LabelCount.Text = "You clicked 0 times";
+            //ButtonClick.Clicked += ButtonClick_Clicked; <--can be called from c# codebehind or xaml itself, useful to call in xaml as you know what event each element is pointing to
+        }
+
+        int count = 0;
+
+        private void ButtonClick_Clicked(object sender, EventArgs e)
+        {
+            //can check which button triggered method with var button = sender as Button   button?.
+            count++;
+            LabelCount.Text = $"You clicked {count} times";
         }
     }
 }
